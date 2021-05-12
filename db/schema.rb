eae544d556bb129bfc6ad70fa80e7db981f44ad0
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-12 05:17:33 +0000

    [Add] Bookモデル title・opinionテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_114935) do
+ActiveRecord::Schema.define(version: 2021_05_12_051212) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
