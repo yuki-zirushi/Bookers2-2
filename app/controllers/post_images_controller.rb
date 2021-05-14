d441bf9cbb68d03c98cadaeb537a8bc08d431fe1
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-14 00:57:10 +0000

    [Add] post_imageコントローラー create アクション定義

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,15 @@
 class PostImagesController < ApplicationController
   
   def create
+    @post_image = Post.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+  end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:image)
   end
   
 end
