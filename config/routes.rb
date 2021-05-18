3568fc46f6ab95d787bdf43e8c0d3f5a964f755e
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-18 02:41:46 +0000

    [Add] about ルーティング・アクション・ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes/about'
   resources :books
   resources :users
   resources :post_images, only: [:create]
