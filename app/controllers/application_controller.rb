2eebc9027d75d852398fdb92b974f0b254e55ac0
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-21 08:54:37 +0000

    [Add] book indexページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < ActionController::Base
   
     before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def after_sign_in_path_for(current_user)
-    user_path(current_user)
-    
+  def after_sign_in_path_for(resource)
+    books_path(resource)
   end
   
   protected
