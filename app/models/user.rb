8df4c3a38246fe77351aeb409447d17dfa9eb782
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-21 22:43:40 +0000

    [Fix] PostImageモデルの削除・UserとBookのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-         has_many :post_images, dependent: :destroy
+         has_many :s, dependent: :destroy
          
 end
