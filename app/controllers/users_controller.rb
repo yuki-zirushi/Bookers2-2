47a45bdcd7bab611180508865461e5ecdc9a6c87
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-22 00:35:01 +0000

    [Add] user edit機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,20 @@ class UsersController < ApplicationController
     redirect_to '/book/#{@book.id}'
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+  
 end
