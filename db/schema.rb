a6aa624925a94b64c693ca3b3f67b81c8674adbe
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-23 06:37:00 +0000

    [Change] カラム名をopinionからbodyに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_053105) do
+ActiveRecord::Schema.define(version: 2021_05_23_063455) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
