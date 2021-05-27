276cfe5e4c61c62848edc39bf29ac69b002752ed
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-27 06:47:43 +0000

    [Update] バリデーション edit エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,11 +36,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      render :edit
     end
-    redirect_to book_path(book.id)
+    
   end
 
   private
