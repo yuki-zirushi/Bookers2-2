61e615fc32e5bc59b206342345c270cc0dd029b4
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-27 08:55:11 +0000

    [Fix] user 削除機能　削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,12 +27,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_path
-  end
-  
   private
   
   def user_params
