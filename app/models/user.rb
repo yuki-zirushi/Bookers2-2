3d00b9aaf920d4fb2682547a95b4dad91ab9d6e0
yuki-zirushi <yuko.12.151@gmail.com>
2021-05-28 09:01:32 +0000

    [Fix] validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   attachment :profile_image
   
-  validates :name, length: { minimum: 2 }, length: { maximum: 50 }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 2 }, length: { maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
 end
